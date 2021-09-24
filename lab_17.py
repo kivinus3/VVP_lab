f550a68e8d0cb91fd332990ba7b98d3e95aac24a
kivinus3 <i10kolpakov@yandex.ru>
2021-09-24 12:06:11 +0300

    done lab 18

--- lab_17.py
+++ lab_17.py
@@ -6,6 +6,7 @@ def getList():
 
 
 def task_1():
+    print("\n1.")
     arr = getList()
     k = int(input())
     l = int(input())
@@ -13,9 +14,11 @@ def task_1():
     for i in range(k, l + 1, 1):
         summ += arr[i]
     print("Результат: " + str((summ / (l - k + 1))))
+    print()
 
 
 def task_2():
+    print("\n2.")
     arr = getList()
     if (len(arr)) < 3:
         print("Результат: 0")
@@ -26,31 +29,39 @@ def task_2():
             print("Результат: 0")
             return
     print("Результат: " + raz)
+    print()
 
 
 def task_3():
+    print("\n3.")
     arr = getList()
     minEl = arr[0]
     for i in range(2, len(arr), 2):
         minEl = min([minEl, arr[i]])
     print("Результат: " + minEl)
+    print()
 
 
 def task_4():
+    print("\n4.")
     arr = getList()
     result = 0
     for i in range(1, len(arr) - 1, 1):
         if (arr[i] > arr[i - 1]) and (arr[i] > arr[i + 1]):
             result = i
     print(result)
+    print()
 
 
 def task_5():
+    print("\n5.")
     arr = getList()
     for i1 in range(0, len(arr) - 1, 1):
         for i2 in range(i1 + 1, len(arr)):
             if arr[i1] == arr[i2]:
                 print("Результат: ", str(i1), str(i2))
+                return
+    print()
 
 
 def main():
---
+++ lab_18.py
@@ -0,0 +1,98 @@
+def getList():
+    result = []
+    for i in range(int(input())):
+        result.append(float(input()))
+    return result
+
+
+def task_1():
+    print("\n1.")
+    arrA = getList()
+    arrB = getList()
+    for i in range(len(arrA)):
+        x1 = arrA[i]
+        x2 = arrB[i]
+        arrA[i] = x2
+        arrB[i] = x1
+    print("Результат:")
+    print(arrA)
+    print(arrB)
+    print()
+
+
+def task_2():
+    print("\n2.")
+    arrA = getList()
+    arrB = []
+    summ = 0
+    for i in range(len(arrA)):
+        summ += arrA[i]
+        arrB.append(summ / (i + 1))
+    print("Результат: " + str(arrB))
+    print()
+
+
+def task_3():
+    print("\n3.")
+    arr = getList()
+    lastOdd = 1
+    for el in arr[::-1]:
+        if el % 2 == 1:
+            lastOdd = el
+            break
+    arr = list(map(lambda x: x if x % 2 == 0 else x * lastOdd, arr))
+    print("Результат: " + str(arr))
+    print()
+
+
+def task_4():
+    print("\n4.")
+    arr = getList()
+    maxN = arr[0]
+    maxI = 0
+    minN = arr[0]
+    minI = 0
+    for i in range(len(arr)):
+        n = arr[i]
+        if n > maxN:
+            maxN = n
+            maxI = i
+        if n < minN:
+            minN = n
+            minI = i
+    first = min([minI, maxI])
+    last = max([minI, maxI])
+    for i in range(first + 1, last):
+        arr[i] = 0
+    print("Результат: " + str(arr))
+    print()
+
+
+def task_5():
+    print("\n5.")
+    arr = getList()
+    if len(arr) < 2:
+        print("Результат: " + str(arr))
+        return
+    first = arr[0]
+    for i in range(len(arr)):
+        n = arr[i]
+        if n > first:
+            arr = arr[1:i] + [first] + arr[i::]
+            break
+    if arr[0] == first:
+        arr = arr[1::]+[first]
+    print("Результат: " + str(arr))
+    print()
+
+
+def main():
+    task_1()
+    task_2()
+    task_3()
+    task_4()
+    task_5()
+
+
+if __name__ == '__main__':
+    main()
